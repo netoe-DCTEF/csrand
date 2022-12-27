4bacee6a5037030e4e833e44a2a5dbc26aa514bb
Ely Torres Neto <93855901+netoe-DCTEF@users.noreply.github.com>
2022-12-27 08:49:13 -0300

    Add files via upload

---
+++ RandForCs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+                                            MADE BY ELY NETO 27/12/2022
+ */
+namespace RandForCs
+{
+    class Rand
+    {
+        List<int> drawnNumbers = new List<int>();
+
+        void Start()
+        {
+            clearList();
+        }
+        public void clearList()
+        {
+            drawnNumbers.Clear();
+        }
+
+        public int generateNumbers(int limit)
+        {
+            System.Random random = new System.Random();
+            int drawn;
+            
+            do
+            {
+                drawn = random.Next(0,limit);
+            }
+            while (drawnNumbers.Contains(drawn));
+
+            drawnNumbers.Add(drawn);
+             
+            return drawn;
+        }
+    }
+}
